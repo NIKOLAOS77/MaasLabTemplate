357d6437b72229fb28b27129c5692c91fa067b97
NIKOLAOS77 <nbalotis@gmail.com>
2024-05-06 14:11:34 +0300

    Initial commit

--- android/app/src/main/java/com/maaslabtemplate/MainActivity.kt
+++ android/app/src/main/java/com/maaslabtemplate/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.maaslabtemplate
+import android.os.Bundle; // added for splash screen
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen;
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this);  // add this
+    super.onCreate(null)
+  }
+
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "MaasLabTemplate"
+
+
+
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
